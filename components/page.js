0cbd61c331c070e67d6a6e3a73302008496c6942
mvllow <mvllow@icloud.com>
2019-09-12 16:03:45 -0500

    add page layouts

---
+++ components/page.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+const Page = ({ children }) => (
+  <>
+    <main>{children}</main>
+
+    <style jsx>{`
+      main {
+        padding: 2rem;
+        width: 100%;
+        height: calc(100vh - 4rem);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+      }
+    `}</style>
+
+    <style jsx global>{`
+      @import url("https://fonts.googleapis.com/css?family=Poppins:300,500&display=swap");
+
+      *, *: before, *: after {
+        box-sizing: border-box;
+      }
+
+      html {
+        background: black;
+      }
+
+      body {
+        color: white;
+        background: black;
+        font-family: "Poppins", -apple-system, BlinkMacSystemFont, "Segoe UI",
+          Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji",
+          "Segoe UI Emoji", "Segoe UI Symbol";
+        -webkit-anti-aliased-webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+      }
+
+      h1,
+      h2,
+      h3 {
+        margin: 0;
+      }
+    `}</style>
+  </>
+);
+export default Page;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,55 @@
 import React from "react";
+import Page from "../components/page";
+import { Twitter, GitHub } from "react-feather";
 
-const Index = () => <h1>Future site of mvllow</h1>;
+const Index = () => (
+  <Page>
+    <section>
+      <h2>plant based minimalist</h2>
+
+      <nav>
+        <img src="../static/logo.svg" alt="mvllow logo" width="16px" />
+        <a href="https://twitter.com/mvllow" target="_blank">
+          <Twitter size={20} stroke="#444" strokeWidth={1.5} />
+        </a>
+        <a href="https://github.com/mvllow" target="_blank">
+          <GitHub size={20} stroke="#444" strokeWidth={1.5} />
+        </a>
+      </nav>
+    </section>
+
+    <style jsx>{`
+      h2 {
+        padding: 0.5rem 0 1rem;
+        font-weight: 300;
+      }
+
+      nav {
+        display: flex;
+        align-items: center;
+      }
+
+      nav img {
+        margin-right: 1rem;
+      }
+
+      nav a {
+        line-height: 0;
+      }
+
+      nav a :global(svg) {
+        transition: stroke 150ms ease;
+      }
+
+      nav a:hover :global(svg) {
+        stroke: white;
+      }
+
+      nav a:not(:last-child) {
+        margin-right: 0.75rem;
+      }
+    `}</style>
+  </Page>
+);
 
 export default Index;
