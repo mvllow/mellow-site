4f77fc9612685cb80d65d9d6580f97184fd2af91
mvllow <mvllow@icloud.com>
2019-09-12 22:19:03 -0500

    convert to styled components

--- components/page.js
+++ components/page.js
@@ -1,53 +1,26 @@
 import React from "react";
 import Head from "next/head";
+import styled from "styled-components";
 
 const Page = ({ children }) => (
   <>
     <Head>
       <title>mvllow</title>
+      <link rel="icon" type="image/png" href="/static/favicon.png" />
     </Head>
 
-    <main>{children}</main>
-
-    <style jsx>{`
-      main {
-        padding: 2rem;
-        width: 100%;
-        height: calc(100vh - 4rem);
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-direction: column;
-      }
-    `}</style>
-
-    <style jsx global>{`
-      @import url("https://fonts.googleapis.com/css?family=Poppins:300,500&display=swap");
-
-      *, *: before, *: after {
-        box-sizing: border-box;
-      }
-
-      html {
-        background: black;
-      }
-
-      body {
-        color: white;
-        background: black;
-        font-family: "Poppins", -apple-system, BlinkMacSystemFont, "Segoe UI",
-          Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji",
-          "Segoe UI Emoji", "Segoe UI Symbol";
-        -webkit-anti-aliased-webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-      }
-
-      h1,
-      h2,
-      h3 {
-        margin: 0;
-      }
-    `}</style>
+    <StyleWrapper>{children}</StyleWrapper>
   </>
 );
+
+const StyleWrapper = styled.main`
+  padding: 2rem;
+  width: 100%;
+  height: calc(100vh - 4rem);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
 export default Page;
--- pages/index.js
+++ pages/index.js
@@ -1,55 +1,56 @@
 import React from "react";
-import Page from "../components/page";
 import { Twitter, GitHub } from "react-feather";
+import Page from "../components/page";
+import styled from "styled-components";
 
 const Index = () => (
   <Page>
-    <section>
+    <StyleWrapper>
       <h2>plant based minimalist</h2>
 
       <nav>
         <img src="../static/logo.svg" alt="mvllow logo" width="16px" />
         <a href="https://twitter.com/mvllow" target="_blank">
-          <Twitter size={20} stroke="#444" strokeWidth={1.5} />
+          <Twitter size={20} stroke="#444" strokeWidth={1.25} />
         </a>
         <a href="https://github.com/mvllow" target="_blank">
-          <GitHub size={20} stroke="#444" strokeWidth={1.5} />
+          <GitHub size={20} stroke="#444" strokeWidth={1.25} />
         </a>
       </nav>
-    </section>
+    </StyleWrapper>
+  </Page>
+);
 
-    <style jsx>{`
-      h2 {
-        padding: 0.5rem 0 1rem;
-        font-weight: 300;
-      }
+const StyleWrapper = styled.section`
+  h2 {
+    padding: 0.5rem 0 1rem;
+    font-weight: 300;
+  }
 
-      nav {
-        display: flex;
-        align-items: center;
-      }
+  nav {
+    display: flex;
+    align-items: center;
 
-      nav img {
-        margin-right: 1rem;
-      }
+    img {
+      margin-right: 1rem;
+    }
 
-      nav a {
-        line-height: 0;
-      }
+    a {
+      line-height: 0;
 
-      nav a :global(svg) {
-        transition: stroke 150ms ease;
-      }
-
-      nav a:hover :global(svg) {
+      &:hover svg {
         stroke: white;
       }
 
-      nav a:not(:last-child) {
+      &:not(:last-child) {
         margin-right: 0.75rem;
       }
-    `}</style>
-  </Page>
-);
+    }
+
+    svg {
+      transition: stroke 150ms ease;
+    }
+  }
+`;
 
 export default Index;
