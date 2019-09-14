d6fe37c99d613be2d4040e0f0d439fef0870a83c
mvllow <mvllow@icloud.com>
2019-09-13 20:05:33 -0500

    fix xo errors/warnings

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,16 @@
-import React from "react";
-import App from "next/app";
-import { ThemeProvider } from "styled-components";
-import theme from "../theme";
+import React from 'react';
+import App from 'next/app';
+import { ThemeProvider } from 'styled-components';
+import theme from '../theme';
 
 export default class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props;
-    return (
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    );
-  }
+	render() {
+		const { Component, pageProps } = this.props;
+
+		return (
+			<ThemeProvider theme={theme}>
+				<Component {...pageProps} />
+			</ThemeProvider>
+		);
+	}
 }
