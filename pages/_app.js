28934caec7af2dabe8e8aaaf30d69dde9f06d042
mvllow <mvllow@icloud.com>
2020-11-20 14:27:09 -0600

    use favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,23 @@
+import Head from 'next/head'
 import { ThemeProvider } from 'next-themes'
 import '../global.css'
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <ThemeProvider
-      attribute='class'
-      defaultTheme='system'
-      disableTransitionOnChange
-    >
-      <Component {...pageProps} />
-    </ThemeProvider>
-  )
-}
+const Meta = () => (
+  <Head>
+    <link rel='icon' type='image/png' href='/favicon.png' />
+    <link rel='stylesheet' href='https://rsms.me/inter/inter.css' />
+  </Head>
+)
+
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider
+    attribute='class'
+    defaultTheme='system'
+    disableTransitionOnChange
+  >
+    <Meta />
+    <Component {...pageProps} />
+  </ThemeProvider>
+)
 
 export default MyApp
