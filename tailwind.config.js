bf8aeed2e6f3d0e300e92ae549026e7fc8c757dc
mvllow <mvllow@icloud.com>
2020-12-02 13:55:12 -0600

    fix font config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'class',
   theme: {
-    fontFamily: {
-      body: ['Inter', 'system-ui', 'sans-serif'],
-      mono: ['monospace'],
-    },
     extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
       colors: {
         primary: '#eb6f92',
+        gray: colors.trueGray,
       },
     },
   },
