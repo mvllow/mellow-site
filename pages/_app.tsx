24e3bbc8eebda8807c1198d04de538e6780cee52
mvllow <mvllow@icloud.com>
2020-12-02 13:29:16 -0600

    add dynamic favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,24 @@
 import type { AppProps } from 'next/app'
 
 import Head from 'next/head'
-import { ThemeProvider } from 'next-themes'
+import { ThemeProvider, useTheme } from 'next-themes'
 import '../global.css'
 
-const Meta = () => (
-  <Head>
-    <title>mellow</title>
-    <link rel='icon' type='image/png' href='/favicon.png' />
-    <link rel='stylesheet' href='https://rsms.me/inter/inter.css' />
-  </Head>
-)
+const Meta = () => {
+  const { resolvedTheme } = useTheme()
+
+  return (
+    <Head>
+      <title>mellow</title>
+      <link
+        rel='icon'
+        type='image/png'
+        href={`/${resolvedTheme || 'dark'}.png`}
+      />
+      <link rel='stylesheet' href='https://rsms.me/inter/inter.css' />
+    </Head>
+  )
+}
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <ThemeProvider
