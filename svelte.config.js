e1f2aab13dc028ab758df0e99ef1c2ae1a71fdc3
mvllow <mvllow@icloud.com>
2022-07-04 20:35:13 -0400

    add tailwindcss

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,7 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess(),
+	preprocess: preprocess({ postcss: true }),
 
 	kit: {
 		adapter: adapter(),
