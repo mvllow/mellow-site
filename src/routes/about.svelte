f042470fd1a144da1c0bb9766f14d4a4e924f830
mvllow <mvllow@icloud.com>
2022-07-04 23:22:49 -0400

    add content to about page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,1 +1,31 @@
-<h1>About</h1>
+<script>
+	import Link from '$lib/link.svelte';
+</script>
+
+<svelte:head>
+	<title>about ~ mellow</title>
+	<meta name="description" content="Question everything." />
+</svelte:head>
+
+<section>
+	<h1>About</h1>
+
+	<p>
+		Practicing minimalism and slower, intentional living. Learning Italian to
+		explore my cultural background and nurture new connections. Exploring low
+		level architecture and self-sustainability. <Link href="/writing"
+			>Documenting my journey</Link
+		>.
+	</p>
+
+	<p>I would love to exchange thoughts and hear your experiences.</p>
+
+	<ul>
+		<li>
+			<Link href="https://eldritch.cafe/web/@not">not@eldritch.cafe</Link>
+		</li>
+		<li>
+			<Link href="https://github.com/mvllow">github.com/mvllow</Link>
+		</li>
+	</ul>
+</section>
