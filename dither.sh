d3c49e96a7e379f29dc4fc16a01a35c13472723e
mvllow <mvllow@icloud.com>
2024-08-25 12:27:37 -0500

    chore: update magick command

--- dither.sh
+++ dither.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-convert "$1" -verbose -format GIF -interlace GIF -resize 640\> -ordered-dither 8x8 -set filename:f "%[t]_dithered" "%[filename:f].gif"
+magick "$1" -verbose -format GIF -interlace GIF -resize 640\> -ordered-dither 8x8 -set filename:f "%[t]_dithered" "%[filename:f].gif"
