35846e2e8a76ffca755508b7be134620ba4a1d7b
mvllow <mvllow@icloud.com>
2025-04-17 22:55:36 -0500

    Update dither.sh

--- dither.sh
+++ dither.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 
+# didder -i "$1" -o "$1_dithered" -p "black white" -r "#242220 #f0eae8" -x 640 bayer 8x8
+
 magick "$1" -verbose -format GIF -interlace GIF -resize 640\> -ordered-dither 8x8 -set filename:f "%[t]_dithered" "%[filename:f].gif"
